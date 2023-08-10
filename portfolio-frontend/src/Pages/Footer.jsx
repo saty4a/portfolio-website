dba734d6259c97405caccb3c715756c45f678434
satyabrata biswal <86743992+saty4a@users.noreply.github.com>
2023-08-10 11:28:13 +0530

    Update Footer.jsx

--- portfolio-frontend/src/Pages/Footer.jsx
+++ portfolio-frontend/src/Pages/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
         <div className="text-center mt-5">
             <div className="flex flex-row gap-5 justify-center">
             <a href="https://github.com/saty4a"><FaGithub /></a>
-            <a href="#"><FaLinkedinIn /></a>
-            <a href="#"><FaMedium /></a>
+            <a href="https://www.linkedin.com/feed/"><FaLinkedinIn /></a>
+            <a href="https://medium.com/@satyabrat53272"><FaMedium /></a>
             <a href="#"><AiFillMail /></a>
             </div>
             <p className="mt-4">Made by Satyabrata Biswal</p>
